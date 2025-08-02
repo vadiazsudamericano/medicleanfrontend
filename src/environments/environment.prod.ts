bc26006efb78bebb7ea17cc178882bf38f353b73
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-08-02 15:57:46 -0500

    Vercel cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backendpiensa-production.up.railway.app'
+  apiUrl: 'https://backend-restablecido-production.up.railway.app'
 };
