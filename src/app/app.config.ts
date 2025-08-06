681f1857994d8a9227ba177538b196e5c0ae2433
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-08-06 00:20:50 -0500

    cambio rol

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
+// RUTA: src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideAnimations(),
+    provideToastr() // ✅ REGISTRA TOASTR GLOBALMENTE
   ]
 };
