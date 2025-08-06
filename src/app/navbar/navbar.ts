8672819aa87b03c42f8aaca6dcf38ad900496507
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-08-05 23:43:03 -0500

    cambios navbar ahora si

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,15 @@
-// navbar.ts
+// RUTA: src/app/navbar/navbar.ts
 
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 import { ThemeService } from '../servicios/theme.service';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css'],
 })
